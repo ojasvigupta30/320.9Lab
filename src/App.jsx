a26faea444d4b43405a4ef15c88c361dfe21d56f
ojasvigupta30 <ojasvigupta30@gmail.com>
2024-11-10 00:49:30 -0600

    Added all the components to the App

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,48 @@ import ToDoList from './components/ToDoList'
 
 
 function App() {
-  
+  const [todos, setToDos] = useState([]);
+
+  // Function to add a new todo
+  function addToDo(todoText) {
+    setToDos([{ text: todoText, complete: false }, ...todos]);
+  }
+
+  // Function to toggle completion status
+  function toggleComplete(index) {
+    const updatedToDos = todos.map(function (todo, id) {
+      return id === index ? { ...todo, complete: !todo.complete } : todo;
+    });
+    setToDos(updatedToDos);
+  }
+
+  // Function to delete a todo
+  function deleteToDo(index) {
+    const updatedToDos = todos.filter(function (_, id) {
+      return id !== index;
+    });
+    setToDos(updatedToDos);
+  }
+
+  // Function to edit a todo
+  function editToDo(index, newText) {
+    const updatedToDos = todos.map(function (todo, id) {
+      return id === index ? { ...todo, text: newText } : todo;
+    });
+    setToDos(updatedToDos);
+  }
+
   return (
-    <>
-    
-    </>
+    <div>
+      <h1>Todo List</h1>
+      <ToDoInput addToDo={addToDo} />
+      <ToDoList
+        todos={todos}
+        toggleComplete={toggleComplete}
+        deleteToDo={deleteToDo}
+        editToDo={editToDo}
+      />
+    </div>
   );
 }
 
